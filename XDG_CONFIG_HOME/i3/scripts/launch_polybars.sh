d7f57a0de786ece93ebce359ed66ec68e74d8b88
Radek Maňák <Radek.Manak@protonmail.com>
2020-01-28 13:43:11 +0100

    i3wm confing change dump

--- XDG_CONFIG_HOME/i3/scripts/launch_polybars.sh
+++ XDG_CONFIG_HOME/i3/scripts/launch_polybars.sh
@@ -6,9 +6,11 @@ killall -q polybar
 sleep 0.2
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+first=true
 for MONITOR in $(polybar --list-monitors | cut -d":" -f1); do
-  if [ "$primary_monitor" = "$MONITOR" ]; then
+  if [ $first = true ]; then
     MONITOR=$MONITOR /usr/bin/polybar primary -r&
+    first=false
   else
     MONITOR=$MONITOR /usr/bin/polybar other -r&
   fi
